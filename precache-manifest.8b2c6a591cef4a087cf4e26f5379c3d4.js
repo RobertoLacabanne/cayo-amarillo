0e932dad4a9e7a85d798eac793e1e0a7d55b9659
Roberto Lacabanne <rlacabanne14@gmail.com>
2022-09-02 18:57:48 -0300

    Updates

---
+++ precache-manifest.8b2c6a591cef4a087cf4e26f5379c3d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b44dc21916f0677293d034cebc63836",
+    "url": "/RobertoLacabanne/cayo-amarillo/index.html"
+  },
+  {
+    "revision": "14fbab572730bebe059d",
+    "url": "/RobertoLacabanne/cayo-amarillo/static/js/2.b3544941.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RobertoLacabanne/cayo-amarillo/static/js/2.b3544941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee847432a885fe8707be",
+    "url": "/RobertoLacabanne/cayo-amarillo/static/js/main.9cc079e3.chunk.js"
+  },
+  {
+    "revision": "237592f91e1d1759404e",
+    "url": "/RobertoLacabanne/cayo-amarillo/static/js/runtime-main.cb7c2673.js"
+  }
+]);
